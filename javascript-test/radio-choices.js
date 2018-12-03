c945cd0ef5f9ab383e1e388580ef0a6990df323c
catbrosius <catherine.brosius@bellevuecollege.edu>
2018-12-02 21:26:16 -0800

    completed radio choices assignment

---
+++ javascript-test/radio-choices.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+const myForm = document.getElementById('sort-type');
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = document.querySelector('input[name="sorter"]:checked').value;
+    if (value.toLowerCase() === 'green') {
+        setColor("greenBackground");
+        showChoice(value);
+    } else if (value.toLowerCase() === 'blue') {
+        setColor("blueBackground");
+        showChoice(value);
+    } else if (value.toLowerCase() === 'red') {
+        setColor("redBackground");
+        showChoice(value);
+    } else if (value.toLowerCase() === 'yellow') {
+        setColor("yellowBackground");
+        showChoice(value);
+    } else if (value.toLowerCase() === 'purple') {
+        setColor("purpleBackground");
+        showChoice(value);
+    }
+});
+let currentClass = null;
+
+const setColor = (className) => {
+    console.log("SET CLASS TO", className);
+    var element = document.getElementById("form-section");
+    if (currentClass) {
+        element.classList.remove(currentClass);        
+    }
+    currentClass = className;
+    element.classList.add(className);
+}
+}
+
+function showChoice (value) {
+    console.log(value);
+    const userDisplay = document.getElementById('user-choice');
+    userDisplay.textContent = value;
+}
